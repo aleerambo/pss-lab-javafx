19653ffc9c70713fbb00315dbbad9fcc3a385ccb
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2024-12-03 16:24:01 +0100

    UPDATE App class

--- javafx-1-property/src/main/java/it/unibo/javafx/property/App.java
+++ javafx-1-property/src/main/java/it/unibo/javafx/property/App.java
@@ -15,6 +15,22 @@ public class App extends Application {
         final VBox mainPane = new VBox();
         final Counter counter = new Counter();
         // TODO aggiungere gli elementi come descritto da readme
+        final Label lbl = new Label();
+        lbl.textProperty().bind(counter.counterProperty().asString());
+        
+        final Button btnPlus = new Button("+");
+        btnPlus.setMinWidth(100);
+        btnPlus.setOnAction(event -> counter.increment());
+        
+        final Button btnMinus = new Button("-");
+        btnMinus.setMinWidth(100);
+        btnMinus.setOnAction(event -> counter.decrement());
+        
+        final HBox buttonBox  = new HBox();
+        buttonBox.getChildren().addAll(btnPlus, btnMinus);
+
+        mainPane.getChildren().addAll(lbl, buttonBox);
+        
         primaryStage.setScene(new Scene(mainPane));
         primaryStage.show();
     }
